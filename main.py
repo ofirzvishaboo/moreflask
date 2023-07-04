1d55df3fc258044cc53ca55c6b53c36821fc863b
Ofir <ofirfirst@gmail.com>
2023-07-04 16:26:11 +0300

    day 30 :)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,12 +8,13 @@ def home():
     return render_template("home.html")
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    df = pandas.read_csv("")
-    temperature = df.station(date)
+    filename = "data_small/TG_STAID" + str(station).zfill(6) +".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
